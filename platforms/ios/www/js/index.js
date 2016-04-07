9392839b5dc59227d58ea71a0e1411a6b79815db
nilsonreguan <nilsonreguan@gmail.com>
2016-04-07 15:30:03 -0600

    7
    7

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -48,4 +48,31 @@ var app = {
     }
 };
 
+module.controller('PictureCtrl', function($scope, $cordovaCamera) {
+
+  document.addEventListener("deviceready", function () {
+
+    var options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 100,
+      targetHeight: 100,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false,
+	  correctOrientation:true
+    };
+
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+      var image = document.getElementById('myImage');
+      image.src = "data:image/jpeg;base64," + imageData;
+    }, function(err) {
+      // error
+    });
+
+  }, false);
+});
+
 app.initialize();
